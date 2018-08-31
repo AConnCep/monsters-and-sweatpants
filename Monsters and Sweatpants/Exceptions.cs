9694793138bf59bdce8c664f7f313f411bfbdcf2
AConnCep <austincepalia@gmail.com>
2018-08-30 21:43:54 -0400

    the game files

---
+++ Monsters and Sweatpants/Exceptions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monsters_and_Sweatpants
+{
+    class Exceptions : System.Exception
+    {
+    }
+
+    class UserDiedException : Exception
+    {
+        public override string Message => "You have died!";
+    }
+
+    class AIDiedException : Exception
+    {
+        public override string Message => "Your opponent has died!";
+    }
+
+    class MonsterDiedException : Exception
+    {
+
+    }
+    class UserKilled : Exception
+    {
+        public override string Message => "You killed the monster!";
+    }
+
+    class AIKilled : Exception
+    {
+        public override string Message => "Your opponent killed the monster!";
+    }
+}
---
+++ Monsters and Sweatpants/MonsterGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monsters_and_Sweatpants
+{
+    static class MonsterGenerator
+    {
+        public static IMonsterable GenerateMonster()
+        {
+            Random r = new Random();
+            int choice = r.Next(0, 4);
+            Console.WriteLine();
+            Console.WriteLine("~~~~GAME BEGIN~~~~");
+            switch (choice)
+            {
+                case 0:                 
+                    return new GoblinOfGoo();
+                case 1:
+                    return new TrollOfTribulation();
+                case 2:
+                    return new OrcOfOrdeal();
+                case 3:
+                    return new DragonOfDeath();
+                // just to get the compiler to shut up
+                default:
+                    return null;
+               
+            }
+        }
+    }
+}
